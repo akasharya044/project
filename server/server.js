6a16f7e8a7834fa0a72f73c8607a628ac0ab5d4c
Akash Kumar <46622841+akasharya044@users.noreply.github.com>
2023-04-30 18:53:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -40,21 +40,21 @@ app.get("*", function (_, res) {
     );
 });
 
-io.on('connection', (socket) => {
-    socket.on('join room', ({room1, room2}) => {
-        socket.join(room1)
-        socket.join(room2)
-    })
-    socket.on("private message", (message) => {
-        io.to(message.room).emit('new Message', {
-            message: message.message,
-            sender: message.sender
-        });
-   })
-    socket.on('disconnect', function () {
-        console.log('Socket disconnected');
-    })
-})
+// io.on('connection', (socket) => {
+//     socket.on('join room', ({room1, room2}) => {
+//         socket.join(room1)
+//         socket.join(room2)
+//     })
+//     socket.on("private message", (message) => {
+//         io.to(message.room).emit('new Message', {
+//             message: message.message,
+//             sender: message.sender
+//         });
+//    })
+//     socket.on('disconnect', function () {
+//         console.log('Socket disconnected');
+//     })
+// })
 
 
 let _response = {}
